b72ae9c7cdbfba4171ef33cff54ce527d61c0ce0
Kimaya25 <65022299+Kimaya25@users.noreply.github.com>
2020-10-28 16:39:44 +0530

    Add files via upload

---
+++ story.js
@@ -0,0 +1,12 @@
+import * as react from "React";
+import {text} from "React-native";
+
+export default class story extends React.Component{
+   render(){
+       return(
+           <View>
+               <Text> Hello and welcome to the hospital.We are here to make sure that you have a speedy recovery. We aspire to fullfill all of your needs and if you need or want anything, just call a nurse and she will help you. To finish your treatment and to get you a speedy recovery we will need you to cooperate with us and listen to us at all times. Hope this visit goes flawlessly for you and you have a speedy recovery.</Text>
+           </View>
+       )
+   }
+}
